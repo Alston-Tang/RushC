79d248a4c1aafd289c352c269ddc344945280389
Alston-Tang <Alston-Tang@users.noreply.github.com>
2025-02-12 17:24:17 +0800

    add renew for refreshing cookie

---
+++ RushC/src/renew.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+use anyhow::Error;
+use structopt::StructOpt;
+use tracing::info;
+use tracing_subscriber::fmt;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use biliup::uploader::credential::login_by_cookies;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(parse(from_os_str), long)]
+    config: PathBuf,
+}
+
+async fn renew(config: PathBuf) -> Result<(), Error> {
+    info!("Attempting to renew credentials...");
+    let bili = login_by_cookies(&config).await?;
+    let info = bili.my_info().await?;
+    info!("Cookie for user {} refreshed", info["data"]["name"]);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::registry().with(fmt::layer()).init();
+    let opts = Opts::from_args();
+
+    renew(opts.config).await?;
+    
+    Ok(())
+} 
